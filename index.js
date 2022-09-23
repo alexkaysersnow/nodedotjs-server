541a03a9a0bf0bb02492d4a53486f146229080aa
Alex K <91154393+alexkaysersnow@users.noreply.github.com>
2022-09-23 10:59:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+const express = require('express');
+var cors = require('cors')
+
+dotenv.config()
+
+const data = JSON.stringify({
+  "action": "create",
+  "version": "0.1.0",
+  "key": process.env.CUMUL_KEY,
+  "token": process.env.CUMUL_TOKEN,
+  "properties": {
+    "integration_id": process.env.INTEGRATION_ID,
+    "type": "sso",
+    "expiry": "24 hours",
+    "inactivity_interval": "1 year",
+    "username": process.env.USER_USERNAME,
+    "name": process.env.USER_NAME,
+    "email": process.env.USER_EMAIL,
+    "suborganization": process.env.USER_SUBORGANIZATION,
+    "role": "editor"
+  }
+});
+
+var config = {
+  method: 'post',
+  url: `${process.env.API_URL || 'https://api.cumul.io'}/0.1.0/authorization`,
+  headers: { 
+    'Content-Type': 'application/json'
+  },
+  data : data
+};
+
+
+
+const app = express();
+const port = 4001;
+app.use(cors());
+
+app.get('/', (req, res) => {
+  axios(config)
+  .then(function (response) {
+    const resp = {
+      status: 'success',
+      key: response.data.id,
+      token: response.data.token
+    };
+    res.json(resp);
+  })
+  .catch(function (error) {
+    const resp = {
+      status: 'failed',
+      error
+    };
+    res.json(resp);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`CUMUL Server app listening on port ${port}`)
+});
